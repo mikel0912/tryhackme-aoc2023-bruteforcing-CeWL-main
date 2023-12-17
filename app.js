95020be97e25f48c70920f917ec8b1e3ba1d0bff
mikel0912 <mikelmartiniglesias@gmail.com>
2023-12-17 11:16:09 +0100

    Cambios mongo

--- app.js
+++ app.js
@@ -30,46 +30,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware para verificar la autenticación de dos factores
-function verify2FA(req, res, next) {
-  if (req.session.authenticatedWith2FA) {
-    // El usuario ya está autenticado con 2FA
-    return next();
-  } else {
-    // Redirigir a la página de inicio de sesión o mostrar un formulario de 2FA
-    res.redirect('/login');
-  }
-}
-
-// Ruta para generar el secreto y el código QR
-app.get('/generate-2fa', (req, res) => {
-  const secret = speakeasy.generateSecret({ length: 20 });
-
-  qrcode.toDataURL(secret.otpauth_url, (err, data_url) => {
-    req.session.temporarySecret = secret.base32;
-    res.json({ dataURL: data_url });
-  });
-});
-
-// Ruta para verificar los códigos generados por Google Authenticator
-app.post('/verify-2fa', (req, res) => {
-  const { token } = req.body;
-  const verified = speakeasy.totp.verify({
-    secret: req.session.temporarySecret,
-    encoding: 'base32',
-    token: token,
-  });
-
-  if (verified) {
-    req.session.authenticatedWith2FA = true;
-    res.json({ success: true, message: 'Código válido' });
-  } else {
-    res.status(401).json({ success: false, message: 'Código no válido' });
-  }
-});
-
 app.use('/', indexRouter);
-app.use('/users', verify2FA, usersRouter); // Aplicar el middleware solo a rutas de usuarios autenticados
+app.use('/users', usersRouter); // Aplicar el middleware solo a rutas de usuarios autenticados
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const mongojs = require('mongojs')
+const db = mongojs('mongodb://localhost:27017/test', ['sgssi'])
+const speakeasy = require('speakeasy');
 
 
 //username and password
@@ -25,18 +28,88 @@ router.get('/dashboard.php.html', function(req, res, next) {
   }
 });
 
-router.post('/login.php.html', function(req, res, next) {
-  if(req.body.username == myusername && req.body.password == mypassword){
+router.post("/login.php.html", async function (req, res, next) {
+  const userToken = req.body.token; // Assuming you have the user from the request
+  const username = req.body.username;
 
-    req.session.userid=req.body.username;
-    console.log(req.session)
-    res.redirect('/dashboard.php.html');
-}
-else{
-  res.render('login', { message: 'Invalid username or password' });
-}
+  try{
+    const storedSecret = await getUser2FASecret(username); //TODO Get the secret from MongoDB
+    console.log(storedSecret);
+    if (!storedSecret) {
+        return res.status(401).json({ message: '2FA not setup for this user' });
+    }
 
+  const verified = speakeasy.totp.verify({
+    secret: storedSecret,
+    encoding: "base32",
+    token: userToken,
+  });
+
+  console.log("Verified:" + verified);
+
+  if (
+    verified &&
+    req.body.username == myusername &&
+    req.body.password == mypassword
+  ) {
+    req.session.userid = req.body.username;
+    console.log(req.session);
+    res.redirect("/dashboard.php.html");
+  } else {
+    res.render("login", { message: "Invalid username or password" });
+  }
+
+    } catch (error) {
+        res.status(500).json({ message: 'Error during login' });
+    }
+  }
   
+);
+
+router.get("/setup-2fa", (req, res) => {
+  const user = req.query.username; // Assuming you get the username from the request
+  console.log(user);
+  const secret = speakeasy.generateSecret({ length: 20 });
+
+  console.log(secret.base32); // Save this value to your DB for the user
+  try {
+    updateUser2FASecret(user, secret.base32); // TODO Store the secret in MongoDB
+    
+    QRCode.toDataURL(secret.otpauth_url, function (err, data_url) {
+      // Send data_url to the frontend to display as a QR code
+      res.send(`<img src='${data_url}'>`);
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error setting up 2FA" });
+  }
 });
+
+updateUser2FASecret = (username, secret) => {
+  return new Promise((resolve, reject) => {
+    db.sgssi.update(
+      { username: username },
+      { $set: { secret: secret } },
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
+getUser2FASecret = (username) => {
+  return new Promise((resolve, reject) => {
+    db.sgssi.findOne({ username: username }, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.secret);
+      }
+    });
+  });
+}
 
 module.exports = router;
